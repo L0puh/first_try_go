5aca46466653c68e1b991bea1d2275dd58eb43c2
Lopuh <1opuhart@gmail.com>
2023-07-11 17:24:08 +0900

    html templates

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
-
+type Page struct {
+    Title string 
+    Info string 
+}
 func index(w http.ResponseWriter, r *http.Request ) {
-    fmt.Fprint(w, "hello, world\n")
+    pg := Page{Title: "some title", Info: "some random info"}
+    tmpl, _ := template.ParseFiles("index.html")
+    tmpl.Execute(w, pg)
 }
 func main () {
     http.HandleFunc("/", index)
-    http.ListenAndServe(":9091", nil)
+    http.ListenAndServe(":9000", nil)
 }
