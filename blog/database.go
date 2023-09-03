fd77b63d6b6e45b93a84180d054d730c5014ba38
Lopuh <1opuhart@gmail.com>
2023-09-03 16:25:48 +0900

    comments

--- blog/database.go
+++ blog/database.go
@@ -13,8 +13,12 @@ type Post struct {
     Id int
     Title string
     Content string
+    Comments []Comment
 }
-
+type Comment struct {
+    User string
+    Comment string
+}
 func connect_db () {
     conn, _ := godotenv.Read(".env")
     var err error
@@ -64,7 +68,7 @@ func get_posts(limit int) []Post {
     print_err(err)
     for rows.Next() {
         rows.Scan(&id, &title, &content)
-        posts = append(posts, Post{id, title, content}) 
+        posts = append(posts, Post{id, title, content, []Comment{}}) 
     }
     return posts
 }
@@ -77,6 +81,50 @@ func delete_postById(id int ) {
     print_err(err)
 }
 
+
+func get_id(name string) int {
+    rows, err := db.Query(`SELECT id FROM users WHERE username=$1`, name) 
+    defer rows.Close()
+    var id int 
+    print_err(err)
+    for rows.Next() {
+        rows.Scan(&id)
+    }
+    return id
+}
+
+func create_comment(user_id int, post_id int, comment string) {
+    _, err := db.Exec(`insert into comments(user_id, post_id, comment) values($1, $2, $3)`,
+    user_id, post_id, comment)
+    print_err(err)
+    log.Printf("new comment: %s", comment)
+}
+
+func get_comments(post_id int) []Comment{
+    rows, err := db.Query(`SELECT user_id, comment FROM comments WHERE post_id=$1`, post_id) 
+    print_err(err)
+    
+    defer rows.Close()
+    var comments[]Comment 
+    var(user int; comment string)
+
+    for rows.Next() {
+        rows.Scan(&user, &comment)
+        comments = append(comments, Comment{get_name(user), comment})
+    }
+    return comments
+
+}
+func get_name(id int) string {
+    rows, err := db.Query(`SELECT username FROM users WHERE id=$1`, id) 
+    defer rows.Close()
+    var name string 
+    print_err(err)
+    for rows.Next() {
+        rows.Scan(&name)
+    }
+    return name
+}
 func get_post(id int) Post {
     rows, err := db.Query(`SELECT * FROM blog WHERE id=$1`, id) 
     defer rows.Close()
@@ -88,7 +136,7 @@ func get_post(id int) Post {
         rows.Scan(&id_p, &title, &content)
     }
 
-    post := Post{id_p, title, content}
+    post := Post{id_p, title, content, []Comment{}}
     return post
 }
 
--- blog/main.go
+++ blog/main.go
@@ -3,27 +3,34 @@ package main
 import (
 	"html/template"
 	"log"
-    "golang.org/x/crypto/bcrypt" 
 	"net/http"
 	"strconv"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var tmp *template.Template
+var login string
 
 func main () {
     connect_db()
     style := http.FileServer(http.Dir("styles"))
     http.Handle("/styles/", http.StripPrefix("/styles/", style))
+    
+    var err error
+    tmp, err = template.ParseGlob("tmp/*.html")
+    print_err(err)
 
-    tmp, _ = template.ParseGlob("tmp/*.html")
     http.HandleFunc("/", home) 
     http.HandleFunc("/add/", add) 
     http.HandleFunc("/delete/", delete_post) 
     http.HandleFunc("/post/", show_post) 
     http.HandleFunc("/signup/", sign_up) 
     http.HandleFunc("/login/", log_in) 
+    http.HandleFunc("/profile/", profile) 
     log.Fatal(http.ListenAndServe("127.0.0.1:9000", nil))
-    close_db()
+
+    defer close_db()
 }
 
 func print_err(err error) {
@@ -31,6 +38,12 @@ func print_err(err error) {
         log.Fatal(err)
     }
 }
+
+func profile(w http.ResponseWriter, r *http.Request) {
+    tmp.ExecuteTemplate(w, "profile.html", login)
+}
+
+
 
 func sign_up (w http.ResponseWriter, r *http.Request) {
     if r.Method == "POST" {
@@ -56,6 +69,7 @@ func log_in (w http.ResponseWriter, r *http.Request) {
         if (check_exist(username, email)) {
             hash_pass := get_password(username)
             if (bcrypt.CompareHashAndPassword(hash_pass, []byte(password)) == nil) {
+                login = username
                 http.Redirect(w, r, "/", http.StatusFound)
             } 
         }
@@ -94,8 +108,18 @@ func delete_post(w http.ResponseWriter, r *http.Request) {
 }
 
 func show_post(w http.ResponseWriter, r *http.Request) {
-    id := r.URL.Path[len("/post/"):]
+    id   := r.URL.Path[len("/post/"):]
     post := get_post(convert(id))
+    id_post := convert(id) 
+    if r.Method == "POST" && login != "" {
+        id_user := get_id(login)
+        comment := r.FormValue("comment") 
+        create_comment(id_user, id_post, comment)
+        // http.Redirect(w, r, "/", http.StatusFound)
+    }
+    comments := get_comments(id_post)
+    post.Comments = comments
+
     tmp.ExecuteTemplate(w, "post.html", post)
 }
 
